a9b84257628676b76bee669ad04d5101246ff8c9
Abdul samad <abdulsmdpm786@gmail.com>
2026-03-02 22:47:20 +0530

    ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,42 @@
-import { StrictMode } from 'react'
-import ReactDOM from "react-dom/client"
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import Dashboard from './Components/Dashboard.jsx'
-import RootLayout from './Layout/RootLayout.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./Components/Dashboard.jsx";
+import RootLayout from "./Layout/RootLayout.jsx";
 import "./index.css";
-import Notes from './Components/Courses/Notes.jsx'
-import {Provider} from "react-redux"
-import {store} from "./Store/store.js"
-import SingleNote from './Components/Courses/SingleNote.jsx'
-import QuizIntro from './Components/Quiz/QuizIntro.jsx'
-import QuizPage from './Components/Quiz/QuizPage.jsx'
-import Dictionary from './Components/Dictionary/Dictionary.jsx'
+import Notes from "./Components/Courses/Notes.jsx";
+import { Provider } from "react-redux";
+import { store } from "./Store/store.js";
+import SingleNote from "./Components/Courses/SingleNote.jsx";
+import QuizIntro from "./Components/Quiz/QuizIntro.jsx";
+import QuizPage from "./Components/Quiz/QuizPage.jsx";
+import Dictionary from "./Components/Dictionary/Dictionary.jsx";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
 
-
-const router = createBrowserRouter ([
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: "Courses", element: <Notes /> },
+        { path: "Courses/:noteId", element: <SingleNote /> },
+        { path: "Quiz", element: <QuizIntro /> },
+        { path: "Quiz/:quizId", element: <QuizPage /> },
+        { path: "Dictionary", element: <Dictionary /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootLayout />,
+    basename: "/Student-Dashboard",
+  },
+);
 
-    children: [
-      {index: true, element: <Dashboard />},
-      {path: "Courses", element: <Notes />},
-      {path: "Courses/:noteId", element: <SingleNote />},
-      {path: "Quiz", element: <QuizIntro />},
-      {path: "Quiz/:quizId", element: <QuizPage />},
-      {path: "Dictionary", element: <Dictionary />}
-
-    ]
-  }
-])
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}> 
- <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
     </Provider>
-   
   </StrictMode>,
-)
-
-
+);
